a44d5d3033a8b6aab7f6eb38954c3f954d0db97a
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2025-05-16 14:18:38 +0200

    Fix formatting

--- json.go
+++ json.go
@@ -31,7 +31,7 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	}
 	w.WriteHeader(code)
 	_, err = w.Write(dat)
-	if err != nil{
+	if err != nil {
 		log.Fatal("an error occured while writing data to the client")
 	}
 }
